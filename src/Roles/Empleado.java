7b1601a38f3bdbac822708a4f4120477a652d4dd
Juan Cristobal <juanalonzocristobalvillodas@gmail.com>
2025-09-14 22:32:56 -0500

    JuanCristobal_1

--- src/Roles/Empleado.java
+++ src/Roles/Empleado.java
@@ -3,7 +3,23 @@ package Roles;
 
 
 public class Empleado {
-    private String nombre;
+    private String usuario;
     private String contraseña;
-    
+
+    public Empleado(String usuario, String contrasena) {
+        this.usuario = usuario;
+        this.contraseña = contrasena;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getContrasena() {
+        return contraseña;
+    }
+
+    public boolean validarLogin(String usuario, String contrasena) {
+        return this.usuario.equals(usuario) && this.contraseña.equals(contrasena);
+    }
 }
